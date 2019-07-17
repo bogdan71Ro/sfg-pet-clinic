9009ea110a6815c7b1ece8d721a7d86e8236281a
BOGDAN PISAI <bogdan.pisai@gmail.com>
2019-07-17 18:10:52 +0300

    BPI Commit 20190717-02

---
+++ src/test/java/org/uranussoftware/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uranussoftware.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
